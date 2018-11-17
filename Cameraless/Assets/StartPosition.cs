493c807b01725ea12925933c81fc0b1afaf61c9e
andrew-benson <andrew.benson@grahambrown.com>
2018-11-17 13:26:09 +0000

    - Upgraded project - Lighting intensity set to 0

---
+++ Cameraless/Assets/StartPosition.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPosition : MonoBehaviour {
+
+    public Transform startPositon;
+
+	// Use this for initialization
+	void Awake () {
+        transform.position = startPositon.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
